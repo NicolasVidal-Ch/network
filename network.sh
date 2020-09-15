c097650fb894451873136cd1b11a646479f36a2b
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-15 13:16:11 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -7,5 +7,8 @@ echo [device] >> /etc/NetworkManager/NetworkManager.conf
 echo "\n" >> /etc/NetworkManager/NetworkManager.conf
 echo wifi.scan-rand-mac-address=no >> /etc/NetworkManager/NetworkManager.conf
 
+#Kill cron:
+sed -i '/network-net/d' /etc/crontab
+
 #Reboot the Pi:
 reboot
