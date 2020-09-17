187d9235c91152629e687db7c169e922d2d1c1dc
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-17 16:08:03 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,14 +1,22 @@
+
 #Configuration "NetworkManager.conf":
 echo "\n" >> /etc/NetworkManager/NetworkManager.conf
 echo [device] >> /etc/NetworkManager/NetworkManager.conf
 echo "\n" >> /etc/NetworkManager/NetworkManager.conf
 echo wifi.scan-rand-mac-address=no >> /etc/NetworkManager/NetworkManager.conf
 
+#New IP:
+IPRASP=$(sed -e '1q' /etc/oldiprasp)
+IPWLAN=$(ip route | grep 'wlan1' | cut -d ' ' -f9)
+
+echo RPI.$IPWLAN > /etc/hostname
+
+echo 127.0.0.1       localhost > /etc/hosts
+echo 127.0.1.1       RPI.$IPWLAN >> /etc/hosts
+
+#Send IP address on ipraspberry file:
+sed -i -e "s/$IPRASP/$IPWLAN/g" /mnt/servrpi/export/exportrpi/hosts
+echo RPI.$IPWLAN >> /mnt/servrpi/export/exportrpi/hostname
+
 #Kill cron:
-sed -i '/network-net/d' /etc/crontab
-
-#mount
-echo @reboot root mount -a >> /etc/crontab
-
-#Reboot the Pi:
-reboot
+#sed -i '/network-net/d' /etc/crontab
