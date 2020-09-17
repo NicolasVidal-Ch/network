1a396cdb5e8ea6be596d3aed814e25fd507c9e8c
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-17 15:07:02 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,3 +1,12 @@
+#Configuration "NetworkManager.conf":
+echo "\n" >> /etc/NetworkManager/NetworkManager.conf
+echo [device] >> /etc/NetworkManager/NetworkManager.conf
+echo "\n" >> /etc/NetworkManager/NetworkManager.conf
+echo wifi.scan-rand-mac-address=no >> /etc/NetworkManager/NetworkManager.conf
+
+#Kill cron:
+sed -i '/network-net/d' /etc/crontab
+
 #New IP:
 IPWLAN=$(ip route | grep 'wlan1' | cut -d ' ' -f9)
 
@@ -10,14 +19,6 @@ echo 127.0.1.1       RPI.$IPWLAN >> /etc/hosts
 sed -i -e "s/$IPWLAN/g" /mnt/servrpi/export/exportrpi/hosts
 echo RPI.$IPWLAN >> /mnt/servrpi/export/exportrpi/hostname
 
-#Configuration "NetworkManager.conf":
-echo "\n" >> /etc/NetworkManager/NetworkManager.conf
-echo [device] >> /etc/NetworkManager/NetworkManager.conf
-echo "\n" >> /etc/NetworkManager/NetworkManager.conf
-echo wifi.scan-rand-mac-address=no >> /etc/NetworkManager/NetworkManager.conf
-
-#Kill cron:
-sed -i '/network-net/d' /etc/crontab
 #mount
 @reboot root mount -a >> /etc/crontab
 
